3246f73c606daa1a56a5103ffe32ce4c0ba41844
OreX <65088103+orex2121@users.noreply.github.com>
2026-07-02 07:26:10 +0300

    1.0.4

--- INSTALL_DEPS.bat
+++ INSTALL_DEPS.bat
@@ -1,14 +1,14 @@
-@echo off
+﻿@echo off
 chcp 65001 > nul
 set PYTHON_EXE=..\..\..\python_embeded\python.exe
 
-echo Установка GigaAM без изменения текущих зависимостей...
+echo Installing GigaAM without changing current dependencies...
 %PYTHON_EXE% -m pip install "gigaam[longform] @ git+https://github.com/salute-developers/GigaAM.git" --no-deps
 
 echo.
-echo Установка pyannote.audio...
+echo Installing pyannote.audio...
 %PYTHON_EXE% -m pip install pyannote.audio
 
 echo.
-echo Установка завершена.
+echo Installation complete.
 pause
